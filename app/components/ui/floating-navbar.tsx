7c800f51cf9e9db37f455cf1ec40746d824dfc38
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-03-17 17:47:53 -0300

    feat: update to the Floating Navbar component ensuring dropdown options menu

--- app/components/ui/floating-navbar.tsx
+++ app/components/ui/floating-navbar.tsx
@@ -8,14 +8,19 @@ import {
   useScroll,
   useMotionValueEvent,
 } from "framer-motion";
-import Link from "next/link";
 import { cn } from "@/app/lib/utils";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./navbar-menu";
+import { projects } from "@/app/data";
+import MagicButton from "../magic-button";
+import { LuCirclePlus } from "react-icons/lu";
+
+import Link from "next/link";
 
 export const FloatingNav = ({
-  navItems,
   className,
+  navItemsHome,
 }: {
-  navItems: {
+  navItemsHome: {
     name: string;
     link: string;
     icon?: JSX.Element;
@@ -24,6 +29,7 @@ export const FloatingNav = ({
 }) => {
   const { scrollYProgress } = useScroll();
   const [visible, setVisible] = useState(true);
+  const [active, setActive] = useState<string | null>(null);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
     // Check if current is not undefined and is a number
@@ -57,7 +63,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
+          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
           className
         )}
         style={{
@@ -67,8 +73,50 @@ export const FloatingNav = ({
           border: "1px solid rgba(255, 255, 255, 0.125)",
         }}
       >
-        {navItems.map((navItem: any, idx: number) => (
-          <Link
+        <Menu setActive={setActive}>
+          <MenuItem setActive={setActive} active={active} item="Início">
+            <div className="flex flex-col space-y-4 text-sm">
+              {navItemsHome.map((item, index) => (
+                <HoveredLink key={index} href={item.link}>
+                  {item.icon}
+                  {item.name}
+                </HoveredLink>
+              ))}
+            </div>
+          </MenuItem>
+
+          <MenuItem setActive={setActive} active={active} item="Projetos">
+            <div className=" text-sm grid grid-cols-1 lg:grid-cols-2 gap-6 md:gap-10 p-1.5 md:p-4">
+              {projects.map((project, index) => (
+                <ProductItem
+                  key={index}
+                  title={project.title}
+                  projectId={project.id}
+                  src={project.img}
+                  description={project.des}
+                />
+              ))}
+            </div>
+
+            <Link href="/projects" className="flex justify-end w-full">
+              <MagicButton
+                title="Ver Mais Projetos"
+                icon={<LuCirclePlus size={20} />}
+                position="right"
+              />
+            </Link>
+          </MenuItem>
+          <MenuItem setActive={setActive} active={active} item="teste">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/hobby">Hobby</HoveredLink>
+              <HoveredLink href="/individual">Individual</HoveredLink>
+              <HoveredLink href="/team">Team</HoveredLink>
+              <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            </div>
+          </MenuItem>
+        </Menu>
+
+        {/* <Link
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
@@ -77,13 +125,12 @@ export const FloatingNav = ({
           >
             <span className="block sm:hidden">{navItem.icon}</span>
             <span className=" text-sm !cursor-pointer">{navItem.name}</span>
-          </Link>
-        ))}
+          </Link> */}
 
-        <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
+        {/* <button className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full">
           <span>Login</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px" />
-        </button>
+        </button> */}
       </motion.div>
     </AnimatePresence>
   );
