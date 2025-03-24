206c37c635647e2b816541699b2e5929636420b1
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-03-24 18:43:36 -0300

    chore: remove sentry setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
@@ -14,15 +12,4 @@ const nextConfig = {
     },
 };
 
-export default withSentryConfig(nextConfig, {
-    org: "lucas-sousa",
-    project: "javascript-nextjs",
-    silent: !process.env.CI,
-    widenClientFileUpload: true,
-    reactComponentAnnotation: {
-        enabled: true,
-    },
-    tunnelRoute: "/monitoring",
-    disableLogger: true,
-    automaticVercelMonitors: true,
-});
+export default nextConfig;
