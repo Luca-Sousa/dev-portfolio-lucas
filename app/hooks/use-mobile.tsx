e619a8161f540cf98347b16fba32b879e14f5ad4
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-03-26 11:44:03 -0300

    feat: update use mobile hook

--- app/hooks/use-mobile.tsx
+++ app/hooks/use-mobile.tsx
@@ -6,16 +6,24 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
     undefined,
   );
+  const [isClient, setIsClient] = React.useState(false); // Adicionando um estado para verificar se está no cliente
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
+    setIsClient(true); // Setando como true após a renderização no cliente
+
+    if (typeof window !== "undefined") {
+      const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
+      const onChange = () => {
+        setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+      };
+      mql.addEventListener("change", onChange);
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
+      return () => mql.removeEventListener("change", onChange);
+    }
   }, []);
+
+  // Só retorna o valor de isMobile depois de garantir que está no cliente
+  if (!isClient) return false;
 
   return !!isMobile;
 }
