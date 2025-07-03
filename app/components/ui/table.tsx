749efaece608828ba0c1eafd82af07a5e8e069bc
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2025-07-03 17:43:16 -0300

    feat: add to shadcn home dashboard

--- app/components/ui/table.tsx
+++ app/components/ui/table.tsx
@@ -9,7 +9,10 @@ const Table = React.forwardRef<
   <div className="relative w-full overflow-auto">
     <table
       ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
+      className={cn(
+        "w-full caption-bottom text-sm focus:outline-none focus:ring-0 focus:ring-offset-0 [&_*]:focus:outline-none [&_*]:focus:ring-0 [&_*]:focus:ring-offset-0 [&_*]:focus-visible:outline-none [&_*]:focus-visible:ring-0",
+        className,
+      )}
       {...props}
     />
   </div>
@@ -58,7 +61,7 @@ const TableRow = React.forwardRef<
   <tr
     ref={ref}
     className={cn(
-      "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+      "border-b transition-colors hover:bg-muted/50 focus:outline-none focus:ring-0 focus:ring-offset-0 data-[state=selected]:bg-muted",
       className,
     )}
     {...props}
@@ -73,7 +76,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0",
+      "h-12 px-4 text-left align-middle font-medium text-muted-foreground focus:outline-none focus:ring-0 focus:ring-offset-0 [&:has([role=checkbox])]:pr-0",
       className,
     )}
     {...props}
@@ -87,7 +90,10 @@ const TableCell = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <td
     ref={ref}
-    className={cn("p-4 align-middle [&:has([role=checkbox])]:pr-0", className)}
+    className={cn(
+      "p-4 align-middle focus:outline-none focus:ring-0 focus:ring-offset-0 [&:has([role=checkbox])]:pr-0",
+      className,
+    )}
     {...props}
   />
 ));
